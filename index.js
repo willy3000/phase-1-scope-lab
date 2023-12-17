f36e7b82f003e56cc07882e51323660f71e7b0e8
Jillo Wario <willywario0@gmail.com>
2023-12-17 16:35:21 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+var customerName = "bob";
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavouriteCustomer = "Ken";
+
+function changeLeastFavoriteCustomer() {
+  leastFavouriteCustomer = "jillo";
+}
